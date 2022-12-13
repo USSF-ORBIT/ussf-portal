e29bab9550e30610f2fdfa2f15a7669f74c66cbd
Abigail Young <abbyoung@gmail.com>
2022-12-12 16:29:01 -0800

    WIP

--- e2e/playwright/cms/tests/article.spec.ts
+++ e2e/playwright/cms/tests/article.spec.ts
@@ -1,4 +1,5 @@
 import { test as base } from '@playwright/test'
+import path from 'path'
 import {
   fixtures,
   TestingLibraryFixtures,
@@ -28,7 +29,7 @@ test.beforeAll(async () => {
 })
 
 describe('Articles', () => {
-  test('can be created by an author', async ({ page, loginPage }) => {
+  test.only('can be created by an author', async ({ page, loginPage }) => {
     test.slow()
     await loginPage.login('cmsauthor', 'cmsauthorpass')
 
@@ -55,6 +56,17 @@ describe('Articles', () => {
     await page.locator('#slug').fill('test-article-for-playwright')
     await page.locator('#title').fill('My Test Article')
     await page.locator('#preview').fill('This is my test article.')
+
+    // Note that Promise.all prevents a race condition
+    // between clicking and waiting for the file chooser.
+    const [fileChooser] = await Promise.all([
+      // It is important to call waitForEvent before click to set up waiting.
+      page.waitForEvent('filechooser'),
+      // Opens the file chooser.
+      console.log('found teh event'),
+      page.locator('text=Upload').click(),
+    ])
+    await fileChooser.setFiles(path.resolve(__dirname, 'placeholder.png'))
 
     await Promise.all([
       page.waitForNavigation(),
