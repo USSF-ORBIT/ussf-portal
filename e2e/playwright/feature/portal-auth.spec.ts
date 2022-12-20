5aa3209ef2f4e97ed14ca8beec4698f40d7b7be0
Jacob Capps <jcapps@truss.works>
2022-12-20 16:15:07 -0600

    Remove duplicate tests

--- e2e/playwright/feature/portal-auth.spec.ts
+++ e2e/playwright/feature/portal-auth.spec.ts
@@ -91,6 +91,7 @@ describe('Portal authentication', () => {
         '/',
         '/sites-and-applications',
         '/about-us',
+        '/about-us/orbit-blog',
         '/news',
         '/news-announcements',
         '/search',
