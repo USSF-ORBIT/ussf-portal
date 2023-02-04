5daeb666afddf454d0efb8142eebd6508a159364
John Gedeon <john@truss.works>
2023-02-03 22:04:32 -0800

    test: add some helpers for articles in the cms

---
+++ e2e/playwright/models/KeystoneArticle.ts
@@ -0,0 +1,47 @@
+import { expect, BrowserContext, Page } from '@playwright/test'
+
+export class KeystoneArticlePage {
+  readonly page: Page
+  readonly context: BrowserContext
+
+  constructor(page: Page, context: BrowserContext) {
+    this.page = page
+    this.context = context
+  }
+
+  async fillOrbitBlogArticleFields({slug = undefined, title}: {slug?: string, title: string}) {
+    await this.page.locator('label[for="category"]').click()
+    await this.page.keyboard.type('O')
+    await this.page.keyboard.press('Enter')
+
+    if (slug) {
+      await this.page.locator('#slug').fill(`${slug}`)
+    }
+    await this.page.locator('#title').fill(`${title}`)
+    await this.page.locator('#preview').fill('This is my test article.')
+  }
+
+  async createOrbitBlogArticle({slug = undefined, title}: {slug?: string, title: string}) {
+    await this.page.locator('text=Create Article').click()
+
+    await this.fillOrbitBlogArticleFields({slug, title})
+
+    await this.createArticle()
+  }
+
+  async createArticle() {
+    await Promise.all([
+      this.page.waitForNavigation(),
+      this.page.locator('form span:has-text("Create Article")').click(),
+    ])
+  }
+
+  async publishArticle() {
+    await this.page.locator('label:has-text("Published") >> nth=0').click()
+
+    await this.page.locator('button:has-text("Save changes")').click()
+    await expect(
+      this.page.locator('label:has-text("Published") input')
+    ).toBeChecked()
+  }
+}
