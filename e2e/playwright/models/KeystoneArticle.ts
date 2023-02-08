1093774ca1c76d2ab854515eaaf88a267e6ee7d1
John Gedeon <john@truss.works>
2023-02-08 08:32:47 -0800

    feat: add helper for setting publishedDate

--- e2e/playwright/models/KeystoneArticle.ts
+++ e2e/playwright/models/KeystoneArticle.ts
@@ -1,5 +1,6 @@
 import { expect, BrowserContext, Page } from '@playwright/test'
 import { faker } from '@faker-js/faker'
+import { DateTime } from 'luxon'
 
 type ArticleFields = {
   title: string
@@ -52,6 +53,14 @@ export class KeystoneArticlePage {
     await this.createArticle()
   }
 
+  async createInternalNewsArticle(articleFields: ArticleFields) {
+    await this.page.locator('text=Create Article').click()
+
+    await this.fillInternalNewsArticleFields(articleFields)
+
+    await this.createArticle()
+  }
+
   async createArticle() {
     await Promise.all([
       this.page.waitForNavigation(),
@@ -59,8 +68,38 @@ export class KeystoneArticlePage {
     ])
   }
 
-  async publishArticle() {
+  nthNumber(number: number) {
+    if (number > 3 && number < 21) return "th";
+    switch (number % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  }
+
+  async publishArticle({ publishedDate }: { publishedDate?: DateTime } = {}) {
     await this.page.locator('label:has-text("Published") >> nth=0').click()
+    if (publishedDate) {
+      await this.page.locator('text=mm/dd/yyyy').click()
+      await expect(this.page.locator('.rdp')).toBeVisible()
+      // If the current month isn't what was passed in assume it's the next one
+      // not handling dates in the past since that's not the happy path but we may need to at somepoint
+      if (await this.page.locator(`h2:has-text("${publishedDate.toFormat("MMMM yyyy")}")`).isHidden()) {
+        await this.page.locator('[aria-label="Go to next month"]').click()
+      }
+      // Keystone labels the calendar buttons 1010th February for some reason
+      // So using `nth=0` guarantees we find 4th February instead of 4th, 14th, and 24th.
+      const dateFormat = `d'${this.nthNumber(publishedDate.day)}' MMMM`
+      await this.page.locator(`button:has-text("${publishedDate.toFormat(dateFormat)}") >> nth=0`).click()
+      // Wait for the dialog to disappear, otherwise you might not set the time
+      await expect(this.page.locator('.rdp')).toBeHidden()
+      await this.page.locator('input[placeholder="00:00"]').fill(publishedDate.toFormat("HH:mm"))
+    }
 
     await this.page.locator('button:has-text("Save changes")').click()
     await expect(
