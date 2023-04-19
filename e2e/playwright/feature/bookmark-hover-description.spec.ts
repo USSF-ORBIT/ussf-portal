46ca2e15d39d2fdfdd8575bdc028344a41f54c81
Jacob Capps <jcapps@truss.works>
2023-04-19 09:27:22 -0500

    Add test for bookmark description

---
+++ e2e/playwright/feature/bookmark-hover-description.spec.ts
@@ -0,0 +1,37 @@
+import { test as base } from '@playwright/test'
+import {
+  fixtures,
+  TestingLibraryFixtures,
+} from '@playwright-testing-library/test/fixture'
+
+import { LoginPage } from '../models/Login'
+import { portalUser1 } from '../cms/database/users'
+
+type CustomFixtures = {
+  loginPage: LoginPage
+}
+
+const test = base.extend<TestingLibraryFixtures & CustomFixtures>({
+  ...fixtures,
+  loginPage: async ({ page, context }, use) => {
+    await use(new LoginPage(page, context))
+  },
+})
+
+const { describe, expect } = test
+
+describe('Hovering over a bookmark in MySpace', () => {
+  test('can see info tooltip with bookmark description', async ({
+    page,
+    loginPage,
+  }) => {
+    await loginPage.login(portalUser1.username, portalUser1.password)
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+
+    await page.locator('text=vMPF').hover()
+    await page.locator('[data-testid="triggerElement"]').hover()
+    await expect(
+      page.locator('text=View your deployment band and other MPF information.')
+    ).toBeVisible()
+  })
+})
