685238775bb83fbb19259eef557626dc43af1a93
John Gedeon <john@truss.works>
2023-04-20 14:15:05 -0700

    test: add test to check for hover tool tip on sites and apps

--- e2e/playwright/feature/bookmark-hover-description.spec.ts
+++ e2e/playwright/feature/bookmark-hover-description.spec.ts
@@ -35,3 +35,25 @@ describe('Hovering over a bookmark in MySpace', () => {
     ).toBeVisible()
   })
 })
+
+describe('Hovering over a bookmark in Sites and Applications', () => {
+  test('can see info tooltip with bookmark description', async ({
+    page,
+    loginPage,
+  }) => {
+    await loginPage.login(portalUser1.username, portalUser1.password)
+    await expect(page.locator('text=WELCOME, BERNIE')).toBeVisible()
+
+    // Navigate to /sites-and-applications using the side nav
+    await page.locator('text=All sites & applications').click()
+    await expect(
+      page.locator('h2:has-text("Sites & Applications")')
+    ).toBeVisible()
+
+    await page.locator('text=vMPF').first().hover()
+    await page.locator('[data-testid="triggerElement"]').hover()
+    await expect(
+      page.locator('text=View your deployment band and other MPF information.')
+    ).toBeVisible()
+  })
+})
