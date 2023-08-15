b7c7d52cd6efab6c1c53388a2de04c9ffc449150
Jacob Capps <jcapps@truss.works>
2023-08-15 14:59:36 -0500

    Add test

--- e2e/playwright/feature/weather-widget.spec.ts
+++ e2e/playwright/feature/weather-widget.spec.ts
@@ -62,3 +62,37 @@ test('can add/remove Weather widget to My Space', async ({
   await expect(page.locator('text=Weather')).toBeHidden()
   await expect(page.locator('text=85202')).toBeHidden()
 })
+
+test('can only add three Weather widgets', async ({ page, loginPage }) => {
+  await loginPage.login(adminUser.username, adminUser.password)
+
+  await expect(page.locator('text=WELCOME, FLOYD KING')).toBeVisible()
+
+  // Add Weather widget
+  await page.getByRole('button', { name: 'Add widget' }).click()
+  await page.getByRole('button', { name: 'Add weather widget' }).click()
+  await page.getByTestId('weatherWidget_input').fill('90210')
+  await page.getByRole('button', { name: 'Save zip code' }).click()
+
+  await expect(page.locator('text=90210')).toBeVisible()
+
+  // Add Weather widget
+  await page.getByRole('button', { name: 'Add widget' }).click()
+  await page.getByRole('button', { name: 'Add weather widget' }).click()
+  await page.getByTestId('weatherWidget_input').fill('85202')
+  await page.getByRole('button', { name: 'Save zip code' }).click()
+
+  await expect(page.locator('text=85202')).toBeVisible()
+
+  // Add Weather widget
+  await page.getByRole('button', { name: 'Add widget' }).click()
+  await page.getByRole('button', { name: 'Add weather widget' }).click()
+  await page.getByTestId('weatherWidget_input').fill('85001')
+  await page.getByRole('button', { name: 'Save zip code' }).click()
+
+  await expect(page.locator('text=85001')).toBeVisible()
+
+  // Check that Add Weather widget button is disabled
+  await page.getByRole('button', { name: 'Add widget' }).click()
+  await expect(page.locator('text=Add weather widget')).toBeDisabled()
+})
