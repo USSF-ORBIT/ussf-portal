4fea8759215d751daec105d6b9186e9fa3dc2405
John Gedeon <john@truss.works>
2024-01-17 14:54:58 -0800

    add test for displaying user id

---
+++ e2e/playwright/feature/settings.spec.ts
@@ -0,0 +1,37 @@
+import { test as base } from '@playwright/test'
+import { faker } from '@faker-js/faker'
+import {
+  fixtures,
+  TestingLibraryFixtures,
+} from '@playwright-testing-library/test/fixture'
+
+import { LoginPage } from '../models/Login'
+import { portalUser1, adminUser } from '../cms/database/users'
+
+type CustomFixtures = {
+  loginPage: LoginPage
+}
+
+const test = base.extend<TestingLibraryFixtures & CustomFixtures>({
+  ...fixtures,
+  loginPage: async ({ page, context }, use) => {
+    await use(new LoginPage(page, context))
+  },
+})
+
+const { describe, expect } = test
+
+describe('Settings page', () => {
+  test('can see userId and copy it to clipboard', async ({ page, loginPage }) => {
+    await loginPage.login(adminUser.username, adminUser.password)
+    await expect(page.locator('text=WELCOME, FLOYD')).toBeVisible()
+    await page.getByTestId('editName').click()
+    await expect(page.getByText('Your user id is: FLOYD.KING.376144527@testusers.cce.af.mil')).toBeVisible()
+    await page.getByRole('button', { name: 'Copy To Clipboard' }).click()
+    await expect(page.getByText('Copied!')).toBeVisible()
+    await expect(page.getByText('Copied!')).toBeHidden()
+ 
+    // logout of portal user
+    await page.locator('[data-testid="header"] [data-testid="button"]').click()
+  })
+})
