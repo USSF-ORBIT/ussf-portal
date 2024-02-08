118e495d09f304b37c9b9ce846aba8f3e048219a
Abigail Young <abbyoung@gmail.com>
2024-02-08 12:30:25 -0800

    add e2e test for jwt

--- e2e/playwright/feature/jwt-auth.spec.ts
+++ e2e/playwright/feature/jwt-auth.spec.ts
@@ -5,6 +5,7 @@ import {
 } from '@playwright-testing-library/test/fixture'
 import { LoginPage } from '../models/Login'
 import { seedDB } from '../portal-client/database/seedMongo'
+import { testUser1 } from '../portal-client/database/users'
 
 import axios from 'axios'
 type CustomFixtures = {
@@ -21,14 +22,84 @@ test.beforeAll(async () => {
   await seedDB()
 })
 
-describe('JWT Authentication', () => {
-  test.skip('user can get valid token', async () => {
-    // Make an API request
+describe('JWT Authorization', () => {
+  test('existing user can get valid token and make a request', async () => {
+    // Make an API request to get JWT
     const response = await axios.get(
-      'http://localhost:5001/login?userId=KING.FLOYD.376144527'
+      `http://localhost:5001/login?userId=${testUser1.userId}`
     )
-
     expect(response.status).toBe(200)
     expect(response.data).toHaveProperty('token')
+
+    // Make an API request to get a protected resource
+    const token = response.data.token
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }
+    const query = `
+      query getDisplayName {
+        displayName
+      }
+    `
+    const protectedResponse = await axios.post(
+      `http://localhost:3000/api/thirdparty/graphql`,
+      { query },
+      { headers }
+    )
+
+    expect(protectedResponse.status).toBe(200)
+    expect(protectedResponse.data.data.displayName).toBe(testUser1.displayName)
+  })
+
+  test('non-existing user can get a valid token and make a request', async () => {
+    // Make an API request to get JWT
+    const response = await axios.get(
+      `http://localhost:5001/login?userId=NEW.USER.0123456789`
+    )
+    expect(response.status).toBe(200)
+    expect(response.data).toHaveProperty('token')
+
+    // Make an API request to get a protected resource
+    const token = response.data.token
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    }
+    const query = `
+      query getDisplayName {
+        displayName
+      }
+    `
+    const protectedResponse = await axios.post(
+      `http://localhost:3000/api/thirdparty/graphql`,
+      { query },
+      { headers }
+    )
+
+    expect(protectedResponse.status).toBe(200)
+    expect(protectedResponse.data.data.displayName).toBe('NEW.USER.0123456789')
+  })
+
+  test('unauth user cannot make a request to protected API calls', async () => {
+    // Make an API request to get a protected resource
+    const headers = {
+      'Content-Type': 'application/json',
+    }
+    const query = `
+      query getDisplayName {
+        displayName
+      }
+    `
+    const protectedResponse = await axios.post(
+      `http://localhost:3000/api/thirdparty/graphql`,
+      { query },
+      { headers }
+    )
+
+    expect(protectedResponse.status).toBe(200) // GraphQL always returns 200
+    expect(protectedResponse.data.errors[0].message).toBe(
+      'User not authenticated'
+    )
   })
 })
